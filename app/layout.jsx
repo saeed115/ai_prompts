de1aca91843d1013bea24cb02e7b481031641051
saeed115 <saeedmahmoud115@gmail.com>
2023-07-31 23:33:49 +0200

    add theme provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,8 +3,7 @@ import { Inter } from 'next/font/google';
 import '@styles/globals.css';
 import Nav from '@components/Nav';
 import Provider from '@components/Provider';
-
-const inter = Inter({ subsets: ['latin'] });
+import { ThemeProvider } from './ThemeProvider';
 
 export const metadata = {
 	title: 'Promptopia',
@@ -15,15 +14,17 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
 			<body>
-				<Provider>
-					<div className="main">
-						<div className="gradient" />
-					</div>
-					<main className="app">
-						<Nav />
-						{children}
-					</main>
-				</Provider>
+				<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+					<Provider>
+						<div className='main'>
+							<div className='gradient' />
+						</div>
+						<main className='app'>
+							<Nav />
+							{children}
+						</main>
+					</Provider>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
