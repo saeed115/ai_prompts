c55adb5763e86e6bdb026a4a5a387b23a7a54194
saeed115 <saeedmahmoud115@gmail.com>
2023-07-31 23:46:17 +0200

    add formatting

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,19 @@
-import Feed from "@components/Feed";
+import Feed from '@components/Feed';
 
 export default function Home() {
-	return <section className='w-full flex-center flex-col'>
-		<h1 className="head_text text-center">
-			Discover & shear
-			<br className="min-md:hidden" />
+	return (
+		<section className='w-full flex-center flex-col'>
+			<h1 className='head_text text-center'>
+				Discover & shear
+				<br className='min-md:hidden' />
+				<span className='orange_gradient text-center'>AI-Powered prompts</span>
+			</h1>
+			<p className='desc text-center'>
+				Promptopia is an open-source AI prompting tool for modern to discover, share
+				creative prompt
+			</p>
 
-			<span className="orange_gradient text-center">AI-Powered prompts</span>
-		</h1>
-		<p className="desc text-center">
-			Promptopia is an open-source AI
-			prompting tool for modern to discover,
-			share creative prompt
-		</p>
-
-		<Feed />
-	</section>;
+			<Feed />
+		</section>
+	);
 }
